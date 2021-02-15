b4f4cb3a1f6eea3477ea499ad81ceeb14a2d102c
Chenguang Wang <w3cing@gmail.com>
2021-02-14 22:57:15 -0800

    proto parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,16 @@
 module Main where
 
+import qualified Data.ByteString as BS
+import Data.ProtoLens (parseMessage)
+import Data.ProtoLens.Encoding.Parser (Parser, runParser)
 import Lib
-import Proto.Ir
+import Proto.Ir as IR
+import Text.Pretty.Simple (pPrint)
 
 main :: IO ()
-main = someFunc
+main = do
+  bs <- BS.getContents
+  ir <- case runParser (parseMessage :: Parser IR.IrModule) bs of
+    Left msg -> fail msg
+    Right ir -> return ir
+  pPrint ir
